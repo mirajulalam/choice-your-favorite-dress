fff45ddad3698689b08d6cf9b0534e56ac9fef81
mirajul alam <alifmirajulalam@gmail.comm>
2022-04-04 11:37:08 +0600

    home details added

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -2,8 +2,10 @@ import React from 'react';
 import './Blogs.css';
 const Blogs = () => {
     return (
-        <div>
-            <h1>This is blogs</h1>
+        <div className='blogs-container'>
+            <h1>React Question</h1>
+            <h2>Question No1 : What is context api?</h2>
+            <p>The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier.The Context API is a React structure that enables you to exchange unique details and assists in solving prop-drilling from all levels of your application.React.createContext() is all you need. It returns a consumer and a provider. Provider is a component that as it's names suggests provides the state to its children. It will hold the "store" and be the parent of all the components that might need that store. Consumer as it so happens is a component that consumes and uses the state.</p>
         </div>
     );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './Home.css';
+import headerImage from '../../Assets/dress.jpg';
 const Home = () => {
     return (
-        <div>
-            <h1>This is home</h1>
+        <div className='home-container'>
+            <div className="home-details">
+                <h1>Choice your favorite dress</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br /> At inventore corrupti, atque distinctio id quasi officiis suscipit similique iure minima.
+                </p>
+                <button>Live Demo</button>
+            </div>
+            <div className="home-images">
+                <img src={headerImage} alt="" />
+            </div>
         </div>
     );
 };
